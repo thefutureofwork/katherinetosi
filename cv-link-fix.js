24bf0e887eeb29aad6a39dccd4ea531b85e47dd8
thefutureofwork <kat@katswindow.net>
2026-09-23 17:31:41 +0800

    Remove temporary build script

--- cv-link-fix.js
+++
@@ -1,4 +0,0 @@
-// This file is a temporary build script to fix cv.pdf links.
-// Run in browser console on each page to verify the fix.
-// All cv.pdf references should now point to cv.html.
-console.log('CV links verified.');
